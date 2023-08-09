1cd8b802402e2f0924aed1d5c3100b2c37caa880
Olly Monger <olivermonger123@gmail.com>
2023-08-10 01:05:51 +0200

    prettier

--- packages/backend/src/handlers/status.ts
+++ packages/backend/src/handlers/status.ts
@@ -1,9 +1,9 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
-	console.log("[STATUS]: Up and running");
-	return {
-		statusCode: 200,
-		body: JSON.stringify({}),
-	};
+  console.log("[STATUS]: Up and running");
+  return {
+    statusCode: 200,
+    body: JSON.stringify({}),
+  };
 };
