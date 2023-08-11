752203af3b772d3fbe03aa823d8aebc52ff39a20
Olly Monger <olivermonger123@gmail.com>
2023-08-11 17:46:59 +0200

    update comment

--- packages/frontend/src/hooks/useIsDesktop/useIsDesktop.ts
+++ packages/frontend/src/hooks/useIsDesktop/useIsDesktop.ts
@@ -3,5 +3,5 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 
 export function useIsDesktop() {
 	const theme = useTheme();
-	return useMediaQuery(theme.breakpoints.up("md")); // Checks to see if the material UI theme currently in use is less than "medium" screen size.
+	return useMediaQuery(theme.breakpoints.up("md")); // Checks to see if the material UI theme currently in use is greater than "medium" screen size.
 }
